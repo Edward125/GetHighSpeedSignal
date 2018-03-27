a2e88b924ca97f1a8ebfd3c2287830c0050f2d96
Edward <edward_song@yeah.net>
2018-03-27 14:56:13 +0800

    create

---
+++ GetHighSpeedSignal/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GetHighSpeedSignal
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoard = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstMsg = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtBoard
+            // 
+            this.txtBoard.Location = new System.Drawing.Point(90, 13);
+            this.txtBoard.Name = "txtBoard";
+            this.txtBoard.Size = new System.Drawing.Size(291, 22);
+            this.txtBoard.TabIndex = 0;
+            this.txtBoard.TextChanged += new System.EventHandler(this.txtBoard_TextChanged);
+            this.txtBoard.DoubleClick += new System.EventHandler(this.txtBoard_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Board File:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.FormattingEnabled = true;
+            this.lstMsg.ItemHeight = 14;
+            this.lstMsg.Location = new System.Drawing.Point(21, 50);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(360, 88);
+            this.lstMsg.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 156);
+            this.Controls.Add(this.lstMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoard);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstMsg;
+    }
+}
+
---
+++ GetHighSpeedSignal/frmMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetHighSpeedSignal
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        #region 参数
+
+
+        //
+        List<string> AllBoardNodes = new List<string>();
+        List<string> AllBoardNCNodes = new List<string>();
+        List<string> AllBoardUsedNodes = new List<string>();
+        //
+        List<string> AllHighSpeedSignal = new List<string>();
+        List<string> HighDDR = new List<string>();
+        List<string> HighVRAM = new List<string>();
+        List<string> HighCNV = new List<string>();
+        List<string> HighSATA = new List<string>();
+        List<string> HighPCIE = new List<string>();
+        List<string> HighHDMI = new List<string>();
+        List<string> HighDP = new List<string>();
+
+
+        #endregion
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoard_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                txtBoard.Text = openfile.FileName;
+                GetBoardNodes(txtBoard.Text.Trim());
+            }
+        }
+
+
+
+        private void GetBoardNodes(string boardfile)
+        {
+
+            //
+            AllBoardNodes.Clear();
+            AllBoardNCNodes.Clear();
+            AllBoardUsedNodes.Clear();
+            //
+
+            string linestr = string.Empty;
+            bool IsNodesStart = false;
+            System.IO.StreamReader sr = new System.IO.StreamReader(boardfile);
+            while (!sr.EndOfStream )
+            {
+                linestr = sr.ReadLine();
+                if (linestr.Trim().ToUpper() == "NODES")
+                    IsNodesStart = true;
+               if (IsNodesStart  &&  linestr .Trim ().ToUpper () =="DEVICES")
+                   IsNodesStart = false ;
+               if (IsNodesStart)
+               {
+                   if (!string.IsNullOrEmpty(linestr) && linestr.EndsWith(";"))
+                   {
+                       AllBoardNodes.Add(linestr.Trim().Replace(";", ""));
+                       if ( linestr.Trim ().ToUpper ().StartsWith ("NC_"))
+                           AllBoardNCNodes.Add(linestr.Trim().Replace(";", ""));
+                       else
+                           AllBoardUsedNodes.Add(linestr.Trim().Replace(";", ""));
+                   }
+               }
+
+            }            
+            sr.Close();
+           lstMsg.Items.Add ("Find all nodes:" + AllBoardNodes.Count + ",NC nodes:" + AllBoardNCNodes.Count + ",Used nodes:" + AllBoardUsedNodes.Count);
+        }
+
+
+        private void GetHighSpeedSignal(List<string> allusednodes)
+        {
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoard_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+    }
+}
